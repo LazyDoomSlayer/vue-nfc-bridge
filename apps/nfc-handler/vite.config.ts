17d5ae8de65aee9b341d8a3ee20f3e3440b077cd
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-02-15 18:18:04 +0100

    feat: update to https

--- apps/nfc-handler/vite.config.ts
+++ apps/nfc-handler/vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), mkcert()],
   server: {
     host: '0.0.0.0',
     strictPort: true,
